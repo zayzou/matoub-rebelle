8c61dc9f165ccc8e46840f644d6c9c024b8f34b0
Zahir SOFFI <zahir.soffi@dti.ulaval.ca>
2026-05-31 19:46:12 -0400

    feat: add Vercel Analytics component and integrate into layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { VercelMonitoring } from "@/components/common/VercelAnalytics";
 
 export const metadata: Metadata = {
   title: {
@@ -61,7 +62,10 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
           rel="stylesheet"
         />
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <VercelMonitoring />
+      </body>
     </html>
   );
 }
---
+++ components/common/VercelAnalytics.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
+export function VercelMonitoring() {
+  return (
+    <>
+      <Analytics />
+      <SpeedInsights />
+    </>
+  );
+}
